3a946e71c8743e7e816a05f56022b3ef8c12ffd4
Robby Grossman <robby@freerobby.com>
2016-04-30 23:24:46 -0400

    Base implementation complete.

--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
 require 'sinatra'
 
 class App < Sinatra::Base
+  configure do
+    @@files = {}
+  end
+
   get '/' do
     'Please use the command line client.'
+  end
+
+  post '/create' do
+    key = SecureRandom.hex
+    @@files[key] = request.body.read
+    key
+  end
+
+  get '/*' do
+    key = params[:splat][0]
+    if @@files[key]
+      @@files.delete(key)
+    else
+      status 400
+      'Key not found or already read.'
+    end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,13 +7,32 @@ RSpec.describe App do
   include Rack::Test::Methods
 
   def app
-    App.new
+    @app ||= App.new
   end
 
   describe 'GET /' do
     it 'responds with 200' do
       get '/'
       expect(last_response).to be_ok
+    end
+  end
+
+  describe 'POST /create' do
+    it 'creates file with body as contents and returns key' do
+      post '/create', 'testing123'
+      key = last_response.body
+      expect(App.class_eval('@@files')[key]).to eql('testing123')
+    end
+  end
+
+  describe 'GET /*' do
+    it 'loads file and deletes it' do
+      post '/create', 'testing123'
+      key = last_response.body
+      get "/#{key}"
+      expect(last_response.body).to eql('testing123')
+      get "/#{key}"
+      expect(last_response.body).not_to eql('testing123')
     end
   end
 end
